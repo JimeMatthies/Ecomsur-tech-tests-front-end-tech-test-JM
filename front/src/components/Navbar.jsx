d2ebff2d25cf0b34c30c20bfa52fafb5a8a89350
Jime Matthies <JimeMatthies@gmail.com>
2023-05-10 12:36:09 -0400

    Fixing Errors

--- front/src/components/Navbar.jsx
+++ front/src/components/Navbar.jsx
@@ -1,12 +1,12 @@
 import React from "react";
-
+import { BsCart4 } from 'react-icons/bs';
 const Navbar = () => {
     return (
         <div className="container-fluid">
            <h1>Navbar</h1>
+           <BsCart4/>
         </div>
     );
 }
-
 
 export default Navbar;
